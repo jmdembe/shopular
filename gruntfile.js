042293f40f33bfbf8434ba80daca23335a236719
Jessica Dembe <jmdembe@gmail.com>
2016-12-03 15:11:32 -0500

    Created new folder to contain source files, fixed mising file location for angular-mocks, moved index to src folder

--- gruntfile.js
+++ gruntfile.js
@@ -2,37 +2,42 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
-        karma: {
-            frameworks: ['mocha', 'chai'],
-            client: {
-                mocha: {
-                    ui: 'bdd'
+        karma: { // task name
+            options: {
+              frameworks: ['mocha', 'chai'],
+              client: {
+                  mocha: {
+                      ui: 'bdd'
+                  }
+              },
+              browsers: ['PhantomJS'],
+              singleRun: true,
+              preprocessors: {
+                  'src/js/**/*.js': ['coverage']
+              },
+              reporters: ['dots', 'coverage'],
+              coverageReporter: {
+                  type: 'text-summary',
+              }
+            },
+            // target name
+            shopular: {
+                options: {
+                    files: [
+                        'node_modules/angular/angular.js',
+                        'node_modules/angular-mocks/angular-mocks.js',
+                        'src/js/shopular.module.js',
+                        'src/js/shopular.controller.js',
+                        'src/js/shopular.service.js',
+                        'test/js/shopular.controller.spec.js'
+                    ]
                 }
-            },
-            browsers: ['PhantomJS'],
-            singleRun: true,
-            preprocessors: {
-                'src/js/**/*.js': ['coverage']
-            },
-            reporters: ['dots', 'coverage'],
-            coverageReporter: {
-                type: 'text-summary',
             }
-        },
+        }
 
-      shopular: {
-          options: {
-              files: [
-                  'node_modules/angular/angular.js',
-                  'node_modules/angular-mocks.js',
-                  'src/js/shopular.module.js',
-                  'src/js/shopular.controller.js',
-                  'src/js/shopular.service.js',
-              ]
-          }
-      }
+
     });
-    grunt.ladnpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('default', ['karma']);
 };
