6a7a3a3af4ba1e70125e0018a1bb72b9cc1ca287
Jessica Dembe <jmdembe@gmail.com>
2016-12-04 22:46:17 -0500

    Wrote a test to see if module gets added

--- test/js/shopular.controller.spec.js
+++ test/js/shopular.controller.spec.js
@@ -1,4 +1,30 @@
 (function() {
     'use strict';
-    
+
+    var expect=chai.expect;
+
+    describe('Shopular controller', function() {
+        var ShopularController;
+        var fakeShopularService = {};
+
+        beforeEach(module('shopular'));
+
+        beforeEach(module(function($provide){
+            $provide.value('ShopularService', fakeShopularService);
+        }));
+
+        beforeEach(inject(function($controller){
+            mockShopularService.getAllItems = function () {
+              return [
+                  {'name': 'pickles', 'price': .22, 'quantity': 2, 'color': 'green', 'discount': 2}
+              ];
+            };
+            ShopularController = $controller('ShouplarController');
+        }));
+      });
+
+        it('Should have the needed scope variables', function() {
+          expect(ShopularController.product).to.be.an('object');
+          expect(Object.keys(ShopularController.product).length).to.equal(0);
+        }); 
 }());
