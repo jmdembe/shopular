a4002d6af702158cd4ab916e737e98a7c9621e7a
Jessica Dembe <jmdembe@gmail.com>
2016-12-31 20:28:26 -0500

    Re-named service to inventory service

---
+++ src/js/inventory.service.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+    angular.module('shopular')
+        .factory('InventoryService', InventoryService);
+
+    function InventoryService() {
+
+      var storeItems = [];
+      var newItem = {};
+
+
+      return {
+          getAllItems: getAllItems,
+          productToAdd: productToAdd,
+          // updateItems: updateItems,
+      }
+
+      function getAllItems() {
+          return inventory;
+      }
+
+      /**
+       * Adds products on submission of the form
+       * @param {Object} product The new product to be added to the list
+       */
+
+      function productToAdd(product) {
+        if (typeof(product)!=='object') {
+          return;
+        }
+        newItem = {
+            id: Date.now(),
+            name: product.name,
+            price: product.price,
+            quantity: product.quantity,
+            color: product.color,
+            discount: product.discount
+          }
+        inventory.push(newItem);
+        return inventory;
+        }
+
+      // function updateItems(id) {
+      //     var found = null;
+      //     storeItem.forEach(function myItem) {
+      //
+      //     }
+      // }
+
+    }
+}());
