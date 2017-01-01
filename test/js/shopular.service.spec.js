3dac5b204a23d568c01faf78ac838e22dd611bce
Jessica Dembe <jmdembe@gmail.com>
2016-12-31 22:46:14 -0500

    Updated script tags as well as input types for numbers in index, updated storage service name in spec file

--- test/js/shopular.service.spec.js
+++ test/js/shopular.service.spec.js
@@ -3,23 +3,23 @@
     var expect = chai.expect;
 
     describe('Shopular controller', function() {
-        var ShopularService;
+        var StorageService;
 
         //first step, create a clean module and bootstrap it.
         beforeEach(module('shopular'));
 
-        beforeEach(inject(function(_ShopularService_) {
-            ShopularService = _ShopularService_;
+        beforeEach(inject(function(_StorageService_) {
+            StorageService = _StorageService_;
         }));
 
         it('Should be able to retrieve an array of inventory items', function (){
-            var result = ShopularService.getAllItems();
+            var result = StorageService.getAllItems();
             expect(result).to.be.an('array');
             expect(result.length).to.equal(12);
         });
 
         it('Should be able to add a new inventory item', function (){
-            var result = ShopularService.productToAdd({
+            var result = StorageService.productToAdd({
               name: 'jello',
               price: .99,
               quantity: 8456785,
@@ -27,7 +27,7 @@
               discount: 0
             });
             expect(result).to.be.an('array');
-            var inventory = ShopularService.getAllItems();
+            var inventory = StorageService.getAllItems();
             expect(inventory[12].name).to.equal('jello');
         });
 
